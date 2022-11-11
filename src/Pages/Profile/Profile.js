a2dacf52525ef641d8f1e904dd3b1a00eac3e40a
Eymen98 <ayman.dashak1998@gmail.com>
2022-11-12 00:57:59 +0300

    first commit

---
+++ src/Pages/Profile/Profile.js
@@ -0,0 +1,12 @@
+import './Profile.css'
+import { GamingLibrary } from '../../sections/index'
+
+const Profile = () => {
+  return (
+    <div>
+      <GamingLibrary/>
+    </div>
+  )
+}
+
+export default Profile
---
+++ src/components/Container/Container.js
@@ -0,0 +1,15 @@
+import './Container.css'
+
+const Container = (props) => {
+    return (
+        <div className='container main-container'>
+            {props.children}
+        </div>
+    )
+}
+
+export default Container
+
+
+
+
